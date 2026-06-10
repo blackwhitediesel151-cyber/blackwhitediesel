1b52627b9662e76ff59a90bc28256f677755f3e3
blackwhitediesel151-cyber <blackwhitediesel151@gmail.com>
2026-06-10 22:30:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPage(pageId){
+document.querySelectorAll('.page').forEach(page=>{
+page.classList.remove('active');
+});
+
+```
+document.getElementById(pageId).classList.add('active');
+```
+
+}
